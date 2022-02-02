cc385d65d8c9fb8e1e0c8e5441af0c0fd4964e3b
NarahK <kimnarah@gmail.com>
2022-02-02 17:25:57 -0500

    style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import Modal from "./Modal";
+import styles from "./css/App.module.css";
+
 //import LoginForm from "./LoginForm";
 
-function App() {
-  const [modalShow, setModalShow] = useState(false);
+function App() { 
 
-  const [user, setUser] = useState({name:"", email:""});
+  const [user, setUser] = useState({name:"", email:""}); //Save user info
+  const [modalShow, setModalShow] = useState(false); // ON&OFF Modal
   
+  // Get user input value
   const onChangeName = (event) => {
     setUser({...user, name:event.target.value})};
-
   const onChangeEmail = (event) => {
     setUser({...user, email:event.target.value})};
 
+   // Off the modal 
   const closeModal = () => {
     setUser({name:"", email:""});
     setModalShow(false)};
@@ -22,25 +25,24 @@ function App() {
     if (user.name ===""||user.email ==="") {
       return;
     }
-    setModalShow(true);
-    
+    setModalShow(true);    
   };
 
     return (
     <div>
       {/* LoginForm */}
-      <form onSubmit={onSubmit} className="login_form">
-        <div className="tag">Limited Offer</div>
-        <div className="greeting">Sign up to get your free trial today!</div>
-        <div className="login_name">
+      <form onSubmit={onSubmit} className={styles.login_form}>
+        <div className={styles.tag}>Limited Offer</div>
+        <div className={styles.greeting}>Sign up to get your free trial today!</div>
+        <div className={styles.form_name}>
           <label htmlFor="name">Name</label>
-          <input onChange={onChangeName} value={user.name} type="text" id="name" placeholder="Enter your Name" />
+          <input onChange={onChangeName} value={user.name} type="text" name="name" id="name" placeholder="Enter your Name" />
         </div>
-        <div className="login_email">
+        <div className={styles.form_email}>
           <label htmlFor="email">Email</label>
-          <input onChange={onChangeEmail} value={user.email} type="email" id="email" placeholder="Enter your Email" />
+          <input onChange={onChangeEmail} value={user.email} type="email" name="email" id="email" placeholder="Enter your Email" />
           </div>
-        <button id="submit_btn">Start My Free Trial</button>
+        <button className={styles.submit_btn}>Start My Free Trial</button>
         </form>
 
 
--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,20 @@
-
+import styles from "./css/Modal.module.css";
+import welcome from "./img/welcome.png"
 
 function Modal ({name, email, closeModal}) {
+        // To get only the domain name from email address
+        const domainString = email.substring(email.lastIndexOf('@') +1);
+        const domainName = domainString.substring(0, domainString.indexOf('.')).toUpperCase();     
+    
     return (
-    <div className="modal">
-        <div className="overlay"></div>
-        <div className="popup">
-          <button onClick={closeModal} id="popup_close">X</button>
-          <div id="popup_img"><img src="" alt="welcome" /></div>
-          <h3 id="popup_greeting">Welcome!{name}</h3>
-          <p id="popup_text">Thanks for filling out the form. A confirmation email has been sent to your {email} account.</p>
-        </div>
+        <div onClick={closeModal} className={styles.modal}>
+            <div className={styles.overlay}></div>
+            <div className={styles.popup}>
+                <button id={styles.popup_close}>X</button>
+                <div id={styles.popup_img}><img src={welcome} alt="welcome" /></div>
+                <h3 id={styles.popup_greeting}>Welcome, {name}!</h3>
+                <p id={styles.popup_text}>Thanks for filling out the form. A confirmation email has been sent to your <span>{domainName}</span> account.</p>
+            </div>
       </div>
     );
     
